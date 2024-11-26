b337d4a5c4e669bd2d7c7fe28eb1670e0dca40ac
Ryan-T-Case <ryan_case@outlook.com>
2024-11-26 13:28:28 -0500

    Successfully scraping a page

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello world');
+import { load } from 'cheerio';
+
+const DIGIMON_CARD_GAME_CARDLIST_BASE_URL =
+  'https://world.digimoncard.com/cardlist';
+enum SetIds {
+  SPECIAL_BOOSTER_VER_2 = '522025',
+}
+
+const scrape = async () => {
+  try {
+    const res = await fetch(
+      `${DIGIMON_CARD_GAME_CARDLIST_BASE_URL}/?search=true&category=${SetIds.SPECIAL_BOOSTER_VER_2}`,
+    );
+    const htmlString = await res.text();
+    console.log(load(htmlString).html());
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+scrape();
