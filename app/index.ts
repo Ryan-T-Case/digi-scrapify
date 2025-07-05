ac9418ae3d16cbbb05a294daf729cac60d248856
Ryan-T-Case <ryan_case@outlook.com>
2025-07-05 17:28:13 -0400

    Scraping data into json

--- app/index.ts
+++ app/index.ts
@@ -1,28 +1,69 @@
 import { load } from 'cheerio';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const DIGIMON_CARD_GAME_CARDLIST_BASE_URL =
-  'https://world.digimoncard.com/cardlist';
+	'https://world.digimoncard.com/cardlist';
 enum SetIds {
-  SPECIAL_BOOSTER_VER_2 = '522025',
+	SPECIAL_BOOSTER_VER_2 = '522025',
 }
 
-const scrape = async () => {
-  try {
-    const res = await fetch(
-      `${DIGIMON_CARD_GAME_CARDLIST_BASE_URL}/?search=true&category=${SetIds.SPECIAL_BOOSTER_VER_2}`,
-    );
-    const htmlString = await res.text();
-    const $ = load(htmlString);
-
-    console.log(
-      $(
-        '.cardlistCol > .image_lists > li.image_lists_item.data > .popup > .card_detail .card_name',
-      ).text(),
-    );
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
+type CardDetails = {
+	id: string;
+	name: string;
 };
 
-scrape();
+const scrapeSet = async (setId: SetIds) => {
+	try {
+		const res = await fetch(
+			`${DIGIMON_CARD_GAME_CARDLIST_BASE_URL}/?search=true&category=${setId}`,
+		);
+		const SetNames = Object.fromEntries(
+			Object.entries(SetIds).map(([key, value]) => [value, key]),
+		);
+		console.log(`Scraping set ${SetNames[setId]}...`);
+
+		const htmlString = await res.text();
+		const $ = load(htmlString);
+
+		const cardListItems = $(
+			'.cardlistCol > .image_lists > li.image_lists_item.data > .popup > .card_detail',
+		);
+		const results: CardDetails[] = [];
+
+		cardListItems.each((_, item) => {
+			const cardId = $(item).find('.cardno').text().trim();
+			const cardName = $(item).find('.card_name').text().trim();
+
+			results.push({
+				id: cardId,
+				name: cardName,
+			});
+		});
+
+		return results;
+	} catch (e) {
+		console.log(e);
+		throw e;
+	}
+};
+
+const scrapeAllSets = async () => {
+	const allResults: Record<string, CardDetails[]> = {};
+
+	for (const [setName, setId] of Object.entries(SetIds)) {
+		allResults[setName] = await scrapeSet(setId);
+	}
+
+	const outputFile = path.join(__dirname, 'cardlist.json');
+	fs.writeFileSync(outputFile, JSON.stringify(allResults, null, 2));
+
+	console.log(`Scraped data saved to ${outputFile}`);
+
+	return allResults;
+};
+
+scrapeAllSets();
